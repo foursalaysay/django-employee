300ae2d3998b52b4aa4f5b3ec26ba2d395cfeea9
kyle-salaysay <foursalaysay@gmail.com>
2024-01-05 02:31:05 +0800

    creating admin views

--- django-final/myproject/myapp/models.py
+++ django-final/myproject/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100)
+    password
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    hire_date = models.DateField()
+
+    # You can add more fields as needed for your Employee model
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
--- django-final/myproject/myapp/views.py
+++ django-final/myproject/myapp/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def admin_view(request):
+    return render(request, 'admin.html', {
+        # PASS DATA
+    })
+    
+def salary_config(request):
+    return render(request, "salary-config.html", {
+        
+    })
+
+def payment_proc(request):
+    return render(request, "payment-proc", {
+        # Return Data
+    })
+    
+def reporting(request):
+    return render(request, "reporting.html", {
+        
+    })
+    
+def system_config(request):
+    return render(request, "tax-rate.html", {
+        
+    })
+    
+
+# <----------------------- EMPLOYEE VIEWS HERE ------------------------->
+def emp_view(request):
+    return render(request, "user.html", {
+        # PASS DATA
+    })
+    
