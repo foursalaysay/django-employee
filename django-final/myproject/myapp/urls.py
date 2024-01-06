e4f434e324f5ca2f99c7f00b682f4a92f077f9b2
kyle-salaysay <foursalaysay@gmail.com>
2024-01-06 20:01:48 +0800

    added user urls

--- django-final/myproject/myapp/urls.py
+++ django-final/myproject/myapp/urls.py
@@ -2,9 +2,23 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("user/home", views.userhome, name="user-home"),
-    path("user/profile-management", views.profile_management_view, name="profile_management_view"),
-    path("user/payroll", views.payroll, name="payroll"),
-    path("user/user-report", views.user-report, name="user-report")
+    path("", views.emp_view, name="home"),
+    path("user/home", views.emp_view, name="emp_view"),
+    path("user/user-stub", views.pay_stub, name="pay_stub"),
+    path("user/doc-repo", views.doc_repo, name="doc_repo"),
+    
+    
+    
+    path("user/profile-management", views.update_info, name="update_info"),
+    path("user/change-pass", views.change_pass, name="change_pass")
+    
+    
+    path("user/payroll", views.user_payroll, name="payroll"),
+    path("user/user-report", views.user_report, name="user_report"),
+    
+    path("user/tax-report", views.user_tax_report, name="user_tax_report"),
+    path("user/financial-sum", views.financial_sum, name="financial_sum"),
+    
+    
+    
 ]
