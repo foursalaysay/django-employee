cf81100ae646372d3687ca37d4b5b2e39825ab5b
kyle-salaysay <foursalaysay@gmail.com>
2024-01-06 19:40:58 +0800

    added urls

--- django-final/myproject/myapp/urls.py
+++ django-final/myproject/myapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("user/home", views.userhome, name="user-home"),
-    path("user/profile-management", views.profile_management_view, name="profile_management_view")
+    path("user/profile-management", views.profile_management_view, name="profile_management_view"),
+    path("user/payroll", views.payroll, name="payroll"),
+    path("user/user-report", views.user-report, name="user-report")
 ]
