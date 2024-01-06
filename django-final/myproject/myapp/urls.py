9a645e794f39ef7f7119f63fbba0c5a4ce83dd4c
kyle-salaysay <foursalaysay@gmail.com>
2024-01-06 19:34:49 +0800

    adding views

--- django-final/myproject/myapp/urls.py
+++ django-final/myproject/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("user/home", views.userhome, name="user-home")
+    path("user/home", views.userhome, name="user-home"),
+    path("user/profile-management", views.profile_management_view, name="profile_management_view")
 ]
